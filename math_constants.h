a4c9c3db6b1d0f791f4f6cd3e293834216689ca7
Praveer Tewari <praveer922@hotmail.sg>
2022-08-18 20:01:31 +0800

    material and metal classes

--- math_constants.h
+++ math_constants.h
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-#include <random>
+#include <cstdlib>
 
 
 // Usings
@@ -25,9 +25,13 @@ inline double degrees_to_radians(double degrees) {
 }
 
 inline double random_double() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    return distribution(generator);
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
 }
 
 inline double clamp(double x, double min, double max) {
